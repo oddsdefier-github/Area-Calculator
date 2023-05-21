792bfd307b2b0acf491f071192f3b0d3b634474c
Odds <panerjeffryjames@gmail.com>
2023-05-21 20:36:30 +0800

    commit

--- js/basic.js
+++ js/basic.js
@@ -2,7 +2,7 @@ const showCalculator = document.getElementById("show-calc");
 const computedArea = document.getElementById("result");
 
 function areaRect(length, width) {
-    if (isNaN(length) || isNaN(width) || length < 0 || width < 0) {
+    if (isNaN(length) || isNaN(width) || length <= 0 || width <= 0) {
         computedArea.innerHTML = `<div class="flex items-center justify-center gap-2 rounded-lg bg-yellow-50 p-4 border border-yellow-200">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="h-5 w-5 stroke-yellow-900">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
